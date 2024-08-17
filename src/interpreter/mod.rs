8d6344233614a89bf7bff453e45e09cd7556f6e9
robinafro <oble.ro.2023@skola.ssps.cz>
2024-08-17 13:28:03 +0200

    Rewrote interpreter to support precompiling

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -66,54 +66,70 @@ impl Interpreter {
         }
     }
 
-    pub fn interpret(&mut self, ast: Vec<ASTNode>) {
+    fn match_node(&mut self, node: ASTNode) -> Vec<Instruction> {
+        match node {
+            ASTNode::VariableDeclaration { mutable, name, value } => {
+                let value = self.compute_expr(*value);
+                self.vm.load_value_into_register(0, value);
+                if mutable {
+                    vec![Instruction::DeclareMutVar(0, name)]
+                } else {
+                    vec![Instruction::DeclareVar(0, name)]
+                }
+            },
+            ASTNode::Assignment { name, kind, value } => {
+                let value = self.compute_expr(*value);
+                let current_value = self.vm.get_variable(&name).expect("Variable not found");
+                let modified_value = match kind {
+                    AssignmentKind::Assign => value,
+                    AssignmentKind::Add => current_value + value,
+                    AssignmentKind::Sub => current_value - value,
+                    AssignmentKind::Mul => current_value * value,
+                    AssignmentKind::Div => current_value / value,
+                    AssignmentKind::Mod => current_value % value,
+                };
+                self.vm.load_value_into_register(0, modified_value);
+                vec![Instruction::StoreVar(0, name)]
+            },
+            // ASTNode::FunctionDeclaration { name, params, body } => {
+            //     // Functions are pre-compiled into instructions that are executed when the function is called
+            //     // For that, we need to support pre-compilation
+                
+            // },
+
+            ASTNode::Identifier(name) => { // The behaviour of this will be in the catch-all case and work for any expression
+                let instructions = vec![
+                    Instruction::LoadVar(0, name),
+                    Instruction::Out(0),
+                ];
+                instructions
+            },
+
+            _ => {
+                println!("{:?}", node);
+                panic!("Not implemented yet");
+            }
+        }
+    }
+
+    pub fn precompile(&mut self, ast: Vec<ASTNode>) -> Vec<Instruction> {
         println!("{:?}", ast);
+
+        let mut instructions = vec![];
 
         // Convert into instructions
         for node in ast {
-            match node {
-                ASTNode::VariableDeclaration { mutable, name, value } => {
-                    let value = self.compute_expr(*value);
-                    self.vm.load_value_into_register(0, value);
-                    let mut instructions = vec![];
-                    if mutable {
-                        instructions.push(Instruction::DeclareMutVar(0, name))
-                    } else {
-                        instructions.push(Instruction::DeclareVar(0, name))
-                    }
-                    self.vm.execute(instructions);
-                },
-                ASTNode::Assignment { name, kind, value } => {
-                    let value = self.compute_expr(*value);
-                    let current_value = self.vm.get_variable(&name).expect("Variable not found");
-                    let modified_value = match kind {
-                        AssignmentKind::Assign => value,
-                        AssignmentKind::Add => current_value + value,
-                        AssignmentKind::Sub => current_value - value,
-                        AssignmentKind::Mul => current_value * value,
-                        AssignmentKind::Div => current_value / value,
-                        AssignmentKind::Mod => current_value % value,
-                    };
-                    self.vm.load_value_into_register(0, modified_value);
-                    let instructions = vec![
-                        Instruction::StoreVar(0, name),
-                    ];
-                    self.vm.execute(instructions);
-                },
+            let node_instructions = self.match_node(node);
+            instructions.extend(node_instructions);
+        }
+        
+        instructions
+    }
 
-                ASTNode::Identifier(name) => {
-                    let instructions = vec![
-                        Instruction::LoadVar(0, name),
-                        Instruction::Out(0),
-                    ];
-                    self.vm.execute(instructions);
-                },
-
-                _ => {
-                    println!("{:?}", node);
-                    panic!("Not implemented yet");
-                }
-            }
+    pub fn interpret(&mut self, ast: Vec<ASTNode>) {
+        for node in ast {
+            let instructions = self.match_node(node);
+            self.vm.execute(instructions);
         }
     }
 }
