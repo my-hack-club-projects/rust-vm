d6b9a1e1180202dd83ae9139f6123f1cdaf9f016
robinafro <oble.ro.2023@skola.ssps.cz>
2024-08-18 19:46:04 +0200

    Added ctrl + c handling

--- src/repl/mod.rs
+++ src/repl/mod.rs
@@ -1,12 +1,30 @@
 use crate::interpreter::Interpreter;
+use std::sync::{Arc, atomic::{AtomicBool, Ordering}};
+use ctrlc;
 
 pub fn start() {
+    let ctrlc_flag = Arc::new(AtomicBool::new(false));
+    let ctrlc_flag_clone = Arc::clone(&ctrlc_flag);
+    
+    ctrlc::set_handler(move || {
+        ctrlc_flag_clone.store(true, Ordering::SeqCst);
+        std::io::Write::write(&mut std::io::stdout(), b"\n> ").unwrap();
+    }).expect("Error setting Ctrl-C handler");
+    
     let mut interpreter = Interpreter::new();
     loop {
         print!("> ");
         std::io::Write::flush(&mut std::io::stdout()).unwrap();
+
         let mut input = String::new();
         std::io::stdin().read_line(&mut input).unwrap();
+        
+        // continue if ctrlc_flag
+        if ctrlc_flag.load(Ordering::SeqCst) {
+            ctrlc_flag.store(false, Ordering::SeqCst);
+            continue;
+        }
+
         let input = input.trim();
 
         if input == "exit" {
